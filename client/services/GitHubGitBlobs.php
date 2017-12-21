8f4c278fcb6761e942f6e5017e34d74c10fd4a71
clickedu-fxescarmis <fxescarmis@clickartweb.com>
2017-12-21 15:03:58 +0100

    new method: createBlob()

--- client/services/GitHubGitBlobs.php
+++ client/services/GitHubGitBlobs.php
@@ -3,22 +3,37 @@
 require_once(__DIR__ . '/../GitHubClient.php');
 require_once(__DIR__ . '/../GitHubService.php');
 require_once(__DIR__ . '/../objects/GitHubBlob.php');
-	
+
 
 class GitHubGitBlobs extends GitHubService
 {
 
-	/**
-	 * Get a Blob
-	 * 
-	 * @return array<GitHubBlob>
-	 */
-	public function getBlob($owner, $repo, $sha)
-	{
-		$data = array();
-		
-		return $this->client->request("/repos/$owner/$repo/git/blobs/$sha", 'GET', $data, 200, 'GitHubBlob', true);
-	}
-	
-}
+    /**
+     * Get a Blob
+     *
+     * @return array<GitHubBlob>
+     */
+    public function getBlob($owner, $repo, $sha)
+    {
+        $data = array();
+        return $this->client->request("/repos/$owner/$repo/git/blobs/$sha", 'GET', $data, 200, 'GitHubBlob', true);
+    }
 
+    /**
+     * Post a Blob
+     *
+     * @param $content string (Required) - The new blob's content.
+     * @param $encoding string (Optional) - The encoding used for content. Currently, "utf-8" and "base64" are supported. Default: "utf-8".
+     *
+     * @return GitHubBlob
+     */
+    public function createBlob($owner, $repo, $content, $encoding="utf-8")
+    {
+        $data = array(
+            'content' => $content,
+            'encoding' => $encoding
+        );
+        $data = json_encode($data);
+        return $this->client->request("/repos/$owner/$repo/git/blobs", 'POST', $data, 201, 'GitHubBlob');
+    }
+}
--- client/services/GitHubGitTrees.php
+++ client/services/GitHubGitTrees.php
@@ -4,33 +4,48 @@ require_once(__DIR__ . '/../GitHubClient.php');
 require_once(__DIR__ . '/../GitHubService.php');
 require_once(__DIR__ . '/../objects/GitHubTree.php');
 require_once(__DIR__ . '/../objects/GitHubTreeExtra.php');
-	
+
 
 class GitHubGitTrees extends GitHubService
 {
 
-	/**
-	 * Get a Tree
-	 * 
-	 * @return GitHubTreeExtra
-	 */
-	public function getTree($owner, $repo, $sha)
-	{
-		$data = array();
-		
-		return $this->client->request("/repos/$owner/$repo/git/trees/$sha", 'GET', $data, 200, 'GitHubTreeExtra');
-	}
-	
-	/**
-	 * Get a Tree Recursively
-	 * 
-	 * @return GitHubTreeExtra
-	 */
-	public function getTreeRecursively($owner, $repo, $sha)
-	{
-		$data = array();
-		
-		return $this->client->request("/repos/$owner/$repo/git/trees/$sha?recursive=1", 'GET', $data, 200, 'GitHubTreeExtra');
-	}
-}
+    /**
+     * Get a Tree
+     *
+     * @return GitHubTreeExtra
+     */
+    public function getTree($owner, $repo, $sha)
+    {
+        $data = array();
+        return $this->client->request("/repos/$owner/$repo/git/trees/$sha", 'GET', $data, 200, 'GitHubTreeExtra');
+    }
 
+    /**
+     * Get a Tree Recursively
+     *
+     * @return GitHubTreeExtra
+     */
+    public function getTreeRecursively($owner, $repo, $sha)
+    {
+        $data = array();
+        return $this->client->request("/repos/$owner/$repo/git/trees/$sha?recursive=1", 'GET', $data, 200, 'GitHubTreeExtra');
+    }
+
+    /**
+     * Post a Tree
+     *
+     * @param $tree array - The different objects to be in this tree
+     * @param $base_tree string - the sha of the base tree
+     *
+     * @return GitHubBlob
+     */
+    public function createTree($owner, $repo, $tree, $base_tree)
+    {
+        $data = array(
+            'tree' => $tree,
+            'base_tree' => $base_tree
+        );
+        $data = json_encode($data);
+        return $this->client->request("/repos/$owner/$repo/git/trees", 'POST', $data, 201, 'GitHubTreeExtra');
+    }
+}
